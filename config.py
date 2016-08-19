4b67046d4922eb1352373b046f6d62720887184f
julichitai <yulyoshka@yandex.ru>
2016-08-19 17:53:03 +0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,41 @@
+from telebot import types
+
+token = '249154043:AAHNGRk1UgHAIS1ZNGmzNeV5v_8e88T9ICo'
+
+locationMSK=types.Location(37.6168444863, 55.7580321449)
+locationSPB=types.Location(30.302746582, 59.9353923257)
+locationBARN=types.Location(83.725373, 53.354609)
+locationBELG=types.Location(36.584772, 50.590782)
+locationVLAD=types.Location(131.915555, 43.125849)
+locationVLIR=types.Location(40.41, 56.153)
+locationVLGRD=types.Location(44.5, 48.73)
+locationVOR=types.Location(39.2051, 51.668)
+locationEKB=types.Location(60.6, 56.83)
+locationIVAN=types.Location(40.978, 56.995)
+locationKAZAN=types.Location(49.11, 55.8)
+locationKLNGRD=types.Location(20.950128, 54.850023)
+locationKRSNDR=types.Location(38.993047, 45.031635)
+locationKRSNRSK=types.Location(92.8932, 56.0153)
+locationMUR=types.Location(33.10051, 68.993489)
+locationNINO=types.Location(44, 56.3)
+locationNOVOKZ=types.Location(87.121125, 53.76268)
+locationNOVOSIB=types.Location(82.930041, 55.010024)
+locationOBN=types.Location(36.626301, 55.104695)
+locationOMSK=types.Location(73.370126, 54.98564)
+locationOREN=types.Location(55.101171, 51.773643)
+locationPERM=types.Location(56.24, 58)
+locationPETR=types.Location(34.35505, 61.794696)
+locationROSDON=types.Location(39.7, 47.2343)
+locationRYAZ=types.Location(39.732868, 54.627376)
+locationSAMAR=types.Location(50.2, 53.2)
+locationSARAT=types.Location(45.989177, 51.522951)
+locationTOMSK=types.Location(84.961936, 56.500828)
+locationHAB=types.Location(135.092665, 48.455892)
+locationCHEL=types.Location(61.41, 55.18)
+locationCHER=types.Location(37.902714, 59.119762)
+
+location=[locationMSK, locationSPB, locationBARN, locationBELG, locationVLAD,locationVLIR,locationVLGRD, locationVOR, locationEKB, locationIVAN, locationKAZAN, locationKLNGRD, locationKRSNDR, locationKRSNRSK, locationMUR, locationNINO, locationNOVOKZ, locationNOVOSIB, locationOBN, locationOMSK, locationOREN, locationPERM, locationPETR, locationROSDON, locationRYAZ, locationSAMAR, locationSARAT, locationTOMSK, locationHAB, locationCHEL, locationCHER]
+city_name=['Москва',"Санкт-Петербург", "Барнаул", "Белгород", "Владивосток" , "Владимир", "Волгоград" , "Воронеж" , "Екатеринбург","Иваново" , "Казань" , "Калининград","Краснодар","Красноярск","Мурманск","Нижний Новгород","Новокузнецк","Новосибирск","Обнинск","Омск","Оренбург","Пермь","Петрозаводск","Ростов-на-Дону","Рязань","Самара","Саратов","Томск","Хабаровск","Челябинск","Череповец"]
+
+item=["Бумага","Стекло","Пластик","Металл","Одежда","Батарейки","Опасные отходы","Иное"]
+callbackdata=["paper","glass","plastic","metall","clothes","battery","danger","other"]
